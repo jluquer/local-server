59005cc4c1ed3bf96fddc5cce167ca5a188c38fd
jluquer <javier.lr96@gmail.com>
2023-04-30 14:08:50 +0200

    replaced react-ts by angular

--- src/components/Links.tsx
+++
@@ -1,8 +0,0 @@
-import { Component, ReactNode } from "react";
-
-export default class Links extends Component {
-  private serverIp = "192.168.1.64";
-  render(): ReactNode {
-    return <a href="{this.serverIp}:8000">Yatch</a>;
-  }
-}
